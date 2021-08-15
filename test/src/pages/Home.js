0f557eeffab377d43b2440db3e1b7c87066c6a8e
marjina-constantin <marjina.costel@mail.ru>
2021-08-15 21:59:45 +0300

    PWA testing

--- test/src/pages/Home.js
+++ test/src/pages/Home.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Home = () => {
 
-    return (
-        <div>
-            Home page
-        </div>
-    );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
+
+  return (
+    <div>
+      {loading ? <img width="150px" alt="loader" src="https://miro.medium.com/max/1400/1*CsJ05WEGfunYMLGfsT2sXA.gif" /> : 'Homepage content loaded'}
+    </div>
+  );
 };
 
 export default Home;
