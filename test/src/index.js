c17f36e97da68653a723f9b9d6d58e351cb660ba
marjina-constantin <marjina.costel@mail.ru>
2021-08-15 20:21:48 +0300

    PWA testing

--- test/src/index.js
+++ test/src/index.js
@@ -15,7 +15,12 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({onUpdate: (registration) => {console.log('new content!')}});
+serviceWorkerRegistration.register({onUpdate: (registration) => {
+        caches.keys().then(function(cacheNames) {
+            console.log(11111, cacheNames);
+        });
+}
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
