0a35e4b3a01cbb392b681c17745a84f09c0e4c49
marjina-constantin <marjina.costel@mail.ru>
2022-02-10 16:13:02 +0200

    Test CI build

--- test/src/pages/Home/index.js
+++ test/src/pages/Home/index.js
@@ -56,7 +56,7 @@ const Home = () => {
           fetchData(token, dataDispatch, data.category);
         }} />
       </Modal>
-      <h2>Expenses</h2>
+      <h2>Expenses 123</h2>
       <h4>Hi, {userDetails?.current_user?.name}!</h4>
       <Filters />
       {noData ? '' :
