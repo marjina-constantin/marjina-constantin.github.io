5d307abc419e5a906f91f5dfcd94f1e7b94fe1cf
marjina-constantin <marjina.costel@mail.ru>
2022-07-27 12:07:52 +0300

    Change charts page lazy loading

--- expenses/src/pages/LazyCharts/index.js
+++ expenses/src/pages/LazyCharts/index.js
@@ -1,10 +1,9 @@
 import React, { Suspense } from "react";
-import img from '../../images/Expenses.png'
 
 const LazyCharts = () => {
   const Charts = React.lazy(() => import("../Charts"));
   return (
-    <Suspense fallback={<img src={img} />}>
+    <Suspense fallback=''>
       <Charts />
     </Suspense>
   );
