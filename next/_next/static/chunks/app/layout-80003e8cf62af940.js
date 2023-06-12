9547b467e8a5d54ac93c3fa0d63a13f52289b26f
Constantin <constantin.marjina@ffw.com>
2023-06-12 21:25:25 +0300

    test next app

--- next/_next/static/chunks/app/layout-80003e8cf62af940.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1208:function(e,n,t){Promise.resolve().then(t.t.bind(t,6476,23)),Promise.resolve().then(t.t.bind(t,4026,23)),Promise.resolve().then(t.t.bind(t,414,23))},4026:function(){}},function(e){e.O(0,[317,667,139,744],function(){return e(e.s=1208)}),_N_E=e.O()}]);
---
+++ next/_next/static/chunks/app/layout-9a9b0ca5f8909360.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9733:function(e,n,t){Promise.resolve().then(t.t.bind(t,4026,23)),Promise.resolve().then(t.t.bind(t,6476,23)),Promise.resolve().then(t.t.bind(t,414,23))},4026:function(){}},function(e){e.O(0,[317,667,139,744],function(){return e(e.s=9733)}),_N_E=e.O()}]);
