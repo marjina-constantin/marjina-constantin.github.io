603bf0879f75eee0f2e8c8243a9f0443158cd9ed
Constantin <constantin.marjina@ffw.com>
2023-06-13 15:23:59 +0300

    test next app

---
+++ next/out/_next/static/chunks/pages/404-ebe3f24b939480c7.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{6141:function(e,r,n){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return n(419)}])},419:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return o}});var t=n(1844);function o(){return(0,t.jsx)("h1",{children:"404 - Page Not Found"})}},9803:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(5784),o=Symbol.for("react.element"),u=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),_=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};r.jsx=function(e,r,n){var t,c={},i=null,s=null;for(t in void 0!==n&&(i=""+n),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(s=r.ref),r)u.call(r,t)&&!f.hasOwnProperty(t)&&(c[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===c[t]&&(c[t]=r[t]);return{$$typeof:o,type:e,key:i,ref:s,props:c,_owner:_.current}}},1844:function(e,r,n){"use strict";e.exports=n(9803)}},function(e){e.O(0,[888,179],function(){return e(e.s=6141)}),_N_E=e.O()}]);
