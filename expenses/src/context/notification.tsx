1564698cfdd6e5643c57a237c4c84b92d7aed224
Sergiu Stici <sergiu.stici@ffw.com>
2024-03-17 14:14:28 +0200

    tsc for notification context

--- expenses/src/context/notification.tsx
+++ expenses/src/context/notification.tsx
@@ -1,20 +1,29 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 import Notification from '../components/Notification';
 import { notificationType, themeList } from '../utils/constants';
 import { useAuthState } from './context';
 
-const NotificationContext = createContext();
+interface NotificationContextProps {
+  children: ReactNode;
+}
+
+const NotificationContext = createContext<
+  (message: string, type: string) => void
+>(() => {});
 
 export const useNotification = () => useContext(NotificationContext);
 
-export const NotificationProvider = ({ children }) => {
-  const [notification, setNotification] = useState(null);
+export const NotificationProvider = ({
+  children,
+}: NotificationContextProps) => {
+  const [notification, setNotification] = useState({ message: '', type: '' });
   let { theme } = useAuthState();
+  // @ts-expect-error TBC
   theme = themeList[theme] ? theme : 'blue-pink-gradient';
   const gradientClass =
     theme === 'blue-pink-gradient' ? 'has-gradient-accent' : '';
 
-  const showNotification = (message, type) => {
+  const showNotification = (message: string, type: string) => {
     setNotification({ message, type });
     let timeout = 2000;
     if (type === notificationType.ERROR) {
@@ -24,7 +33,7 @@ export const NotificationProvider = ({ children }) => {
 
     // Clear the notification after a certain duration (e.g., 3 seconds)
     setTimeout(() => {
-      setNotification(null);
+      setNotification({ message: '', type: '' });
     }, timeout);
   };
 
@@ -34,8 +43,8 @@ export const NotificationProvider = ({ children }) => {
         {children}
         {notification && (
           <Notification
-            message={notification.message}
-            type={notification.type}
+            message={notification!.message}
+            type={notification!.type}
           />
         )}
       </NotificationContext.Provider>
