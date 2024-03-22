9502267ce6d30791b9d1b2fd689ed35c49313d91
Constantin Marjina <constantin.marjina@ffw.com>
2024-03-22 14:44:17 +0200

    Improve performance for charts page filtering

--- expenses/src/index.tsx
+++ expenses/src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 
 registerSW({ immediate: true, scope: '/expenses/' });
 
-const container = document.getElementById('root');
-
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
