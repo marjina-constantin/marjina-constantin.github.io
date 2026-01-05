634f882d50a23127708ebd60b555d9f72ccb202f
Constantin Marjina <constantin.marjina@ffw.com>
2026-01-05 10:43:42 +0200

    Include clickable average for last 7, 14 and 30 days

--- expenses/src/components/LastTwoMonthsAverage.tsx
+++ expenses/src/components/LastTwoMonthsAverage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState, useData } from '../context';
 import { formatNumber } from '../utils/utils';
 import { AuthState, DataState, TransactionOrIncomeItem } from '../type/types';
@@ -6,37 +6,54 @@ import { AuthState, DataState, TransactionOrIncomeItem } from '../type/types';
 export default function LastTwoMonthsAverage() {
   const { data } = useData() as DataState;
   const { currency } = useAuthState() as AuthState;
+  const [daysPeriod, setDaysPeriod] = useState<number>(60);
 
   useEffect(() => {}, [data.raw]);
 
-  let lastTwoMonthsTotal: number = 0;
-  let userHasMoreThanTwoMonths = false;
+  const cyclePeriod = () => {
+    setDaysPeriod(prev => {
+      if (prev === 60) return 7;
+      if (prev === 7) return 14;
+      if (prev === 14) return 30;
+      return 60; // if 30, go back to 60
+    });
+  };
+
+  let periodTotal: number = 0;
+  let userHasMoreThanPeriod = false;
   let lastProcessedItem = {};
-  const twoMonthsAgo = new Date().setDate(new Date().getDate() - 60);
+  const periodStartDate = new Date().setDate(new Date().getDate() - daysPeriod);
+  
   for (const item of data.raw) {
     if ((item as TransactionOrIncomeItem).type === 'incomes') {
       continue;
     }
     const itemDate = new Date((item as TransactionOrIncomeItem).dt);
-    if (itemDate < new Date(twoMonthsAgo)) {
-      userHasMoreThanTwoMonths = true;
+    if (itemDate < new Date(periodStartDate)) {
+      userHasMoreThanPeriod = true;
       break;
     }
     lastProcessedItem = item as TransactionOrIncomeItem;
-    lastTwoMonthsTotal = lastTwoMonthsTotal + parseFloat(item.sum);
+    periodTotal = periodTotal + parseFloat(item.sum);
   }
 
   const timeDiff =
     new Date().getTime() -
     new Date((lastProcessedItem as TransactionOrIncomeItem).dt).getTime();
-  const daysDiff = userHasMoreThanTwoMonths
-    ? 60
+  const daysDiff = userHasMoreThanPeriod
+    ? daysPeriod
     : timeDiff / (1000 * 3600 * 24);
 
   return (
-    <span>
-      Average spending for the last 60 days:{' '}
-      {formatNumber(lastTwoMonthsTotal / Math.ceil(daysDiff))} {currency} / day
+    <span
+      onClick={cyclePeriod}
+      style={{
+        cursor: 'pointer',
+        userSelect: 'none'
+      }}
+    >
+      Average spending for the last {daysPeriod} days:{' '}
+      {formatNumber(periodTotal / Math.ceil(daysDiff))} {currency} / day
     </span>
   );
 }
